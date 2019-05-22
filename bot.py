10faeaf94d5593004e2f5233e9acf9fe764ffcd4
Ilya Kharlamov <ilya-kharlamov2017@ya.ru>
2019-05-22 14:56:45 +0300

    make3

--- bot.py
+++ bot.py
@@ -16,6 +16,5 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
-	bot.send_meassage(message.chat.id, 'Привет')
-
+    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с Хабра')
 bot.polling()
