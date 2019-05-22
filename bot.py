a09667cdde8ff9def6b3270def93e46244567adf
Ilya Kharlamov <ilya-kharlamov2017@ya.ru>
2019-05-22 15:28:00 +0300

    make4

--- bot.py
+++ bot.py
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
-import parser
+from telebot import types
 
-"""bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_meassages(message):
-	bot.send_meassage(message.chat.id, message.text)
-
-if __name__ == '__main__':
-	bot.polling(none_stop = True)  """
 TOKEN = config.TOKEN
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'go'])
-def start_handler(message):
-    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с Хабра')
-bot.polling()
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+	bot.send_message(message.chat.id, message.text)
+
+markup = types.ReplyKeyboardMarkup()
+markup = row('this is buttomn')
+bot.send_message(message.chat.id, 'choose...', reply_markup=markup)
+
+
+
+if __name__ == '__main__':
+	bot.polling(none_stop = True)
+
+
+
