51f9eb56a26e1bbc0f1989ee198e0db96c4bf529
Ilya Kharlamov <ilya-kharlamov2017@ya.ru>
2019-05-22 13:58:14 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import config
+import telebot
+import parser
+
+"""bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_meassages(message):
+	bot.send_meassage(message.chat.id, message.text)
+
+if __name__ == '__main__':
+	bot.polling(none_stop = True)  """
+TOKEN = config.TOKEN
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+	bot.send_meassage(message.chat.id, 'Привет')
+
+bot.polling()
---
+++ config.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# конфиг @ppppppppp99_bot
+TOKEN = '49s'
